53f043f514438fd3d4c1cfaa7b870099f87e9317
Harikrishnan R <106871878+Pixelrick420@users.noreply.github.com>
2024-10-11 15:23:53 +0530

    added source code

---
+++ kryptos.py
@@ -0,0 +1,39 @@
+#kryptos event source code
+
+#guidelines
+'''Guidelines 
+        1. This contest is open to lone wolves or teams of upto 3 participants. 
+        2. A registration fee of ₹50 per head is required.
+        3. This is an event that focuses on the overall technical skillset of a team.
+        4. There will be a set of __ challenges which will be based on Cryptography, Logical gates, Coding and DSA.
+        5. Unlocking one challenge opens up the hint or key to the other.
+        6. It is an entirely offline event, and no access to mobile phone, or internet will be allowed during the game.
+        7. Decision of judges will be final and binding.'''  #this should be formatted and shortened to be printed at the begining of the event
+
+def start(): #call this function after printing guidelines
+    print('WELCOME TO KRYPTOS')
+    print('')#Add somekind of storyline or plot (maybe)
+    q0()
+
+def q0():
+    while True:
+        response = input('Code 0 : ')
+        if response == 'dummycode':
+            break
+    q1()
+    
+def q1():
+    while True:
+        response = input('Code 1 : ')
+        if response == 'dummycode':
+            break
+    q2()
+
+def q2():
+    while True:
+        response = input('Code 2 : ')
+        if response == 'dummycode':
+            break
+    q3()
+    
+    
